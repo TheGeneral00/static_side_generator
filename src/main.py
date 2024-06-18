e1e436a53f5db6b501e0c8c7ad498772e1787941
TheGeneral00 <richard.beldner@outlook.de>
2024-06-18 20:57:06 +0200

    added src/main.sh script plus py code

--- src/main.py
+++ src/main.py
@@ -1,1 +1,6 @@
-print("hello world")
+from textnode import TextNode
+
+def main():
+    TestNode = TextNode("This is a text node", "bold", "https://www.boot.dev")
+    TestNode.__repr__()
+main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,12 +1,13 @@
-class TextNode(text, text_type, url):
-    self.text = text
-    self.text_type = text_type
-    self.url = url
+class TextNode():
+    def __init__(self, text, text_type, url):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
     
-    def __eq__(TextNode):
+    def __eq__(self, TextNode):
         if self.text == TextNode.text and self.text_type == TextNode.tet_type and self.url == TextNode.url:
             return True
         return False
 
-    def __repr__():
-        print(f"TextNode({self.text}, {self.text_type}, {self.url}")
+    def __repr__(self):
+        print(f"TextNode({self.text}, {self.text_type}, {self.url})")
